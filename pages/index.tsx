b115f8df28402c60b6aaa7486ae95b8ed27a332b
EduardoReisUX <ducabruno09@hotmail.com>
2022-01-21 14:22:23 -0400

    add get cookies from login page and redirect to dashboard

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,10 @@
-import type { NextPage } from "next";
+import type { GetServerSideProps } from "next";
+import { parseCookies } from "nookies";
 import { FormEvent, useContext, useState } from "react";
 import { AuthContext } from "../contexts/AuthContext";
 import styles from "../styles/Home.module.css";
 
-const Home: NextPage = () => {
+export default function Home() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
@@ -32,6 +33,21 @@ const Home: NextPage = () => {
       <button type="submit">Entrar</button>
     </form>
   );
-};
+}
 
-export default Home;
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const cookies = parseCookies(ctx);
+
+  if (cookies["nextauth.token"]) {
+    return {
+      redirect: {
+        destination: "/dashboard",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
+};
