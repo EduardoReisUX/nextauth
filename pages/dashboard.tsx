008fec7856fa913394c8841cf255ea22bbeb0989
EduardoReisUX <ducabruno09@hotmail.com>
2022-01-25 19:44:10 -0400

    add Can permission component

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,16 +1,12 @@
 import { useContext, useEffect } from "react";
+import { Can } from "../components/Can";
 import { AuthContext } from "../contexts/AuthContext";
-import { useCan } from "../hooks/useCan";
 import { setupAPIClient } from "../services/api";
 import { api } from "../services/apiClient";
 import { withSSRAuth } from "../utils/withSSRAuth";
 
 export default function dashboard() {
   const { user } = useContext(AuthContext);
-
-  const userCanSeeMetrics = useCan({
-    permissions: ["metrics.list"],
-  });
 
   useEffect(() => {
     api
@@ -22,7 +18,9 @@ export default function dashboard() {
   return (
     <>
       <h1>Dashboard: {user?.email}</h1>
-      {userCanSeeMetrics && <h1>Métricas</h1>}
+      <Can permissions={["metrics.list"]}>
+        <h1>Métricas</h1>
+      </Can>
     </>
   );
 }
